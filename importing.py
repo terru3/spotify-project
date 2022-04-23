dbfede6c15ca45e8b3daff6df509fd5ad704b967
terru3 <terru3@hotmail.com>
2022-04-23 10:34:52 -0700

    formatting revision to dataframes

--- importing.py
+++ importing.py
@@ -22,8 +22,29 @@ def get_track_data(i):
 # for creating the relationship dataframe
 track_slices = [get_track_data(i) for i in range(0, 2000, 1000)]
 # just change 2000 to 1000000 to retrieve all songs
-tracks = pd.concat(track_slices, ignore_index=True)
+tracks_raw = pd.concat(track_slices, ignore_index=True)
+
+# Now we combine duplicates to form a dataframe which contains only unique tracks.
+# For every track, its "playlist_id" column contains a list of all playlists it is
+# contained in:
+tracks = tracks_raw.groupby(["track_name", "artist_name"]).agg({"pos": "first",
+                                                                "track_uri": "first",
+                                                                "artist_uri": "first",
+                                                                "album_uri": "first",
+                                                                "duration_ms": "first",
+                                                                "album_name": "first",
+                                                                "playlist_id": list})
+# "first" means the column is included but remains unchanged
+tracks = tracks.reset_index().reset_index()
+# reset once first to pop out the "track_name" and "artist_name" multiindex as columns,
+# then again to use the new index sequence [0, 1,....] as our "song_id" identifier
+tracks = tracks.rename(columns={"index": "song_id"})
 print(tracks)
+
+# Example of a few entries——we can see that "A Sky Full of Stars - Hardwell Remix" appears
+# in 4 playlists
+print(tracks.loc[1063:1067])
+
 print(tracks["track_name"].value_counts())
 print(tracks["artist_name"].value_counts())
 
@@ -31,40 +52,29 @@ print(tracks["artist_name"].value_counts())
 playlist_slices = [get_playlist_data(i) for i in range(0, 2000, 1000)]
 # just change 2000 to 1000000 to retrieve all data
 playlists = pd.concat(playlist_slices, ignore_index=True)
+playlists = playlists.rename(columns={"pid": "playlist_id"})
 print(playlists)
 print(playlists.info())
 
-# Finally, we need our relationship dataframe.
+# Relationship dataframe
 
-# To do so, we first create a track dataframe which contains only unique tracks.
-# For every track, its "playlist_id" column contains a list of all playlists it is
-# contained in
-tracks_unique = tracks.groupby(["track_name", "artist_name"]).agg({"playlist_id": list})
-tracks_unique = tracks_unique.reset_index().reset_index()
-# reset once first to pop out the "track_name" and "artist_name" multiindex as columns,
-# then again to use the new index sequence [0, 1,....] as our "song_id" identifier
-tracks_unique = tracks_unique.rename(columns={"index": "song_id"})
-print(tracks_unique)
+# Now, all that's left to do is extract the song_id and playlist info into a new
+# "relationship" dataframe. For every song, take its song_id and iterate over its
+# playlist_id to create as many rows as needed.
 
-# Example of a few entries——we can see that "A Sky Full of Stars - Hardwell Remix" appears
-# in 4 playlists
-print(tracks_unique.loc[1063:1067])
-
-# Now, all that's left to do is extract the playlist info into a new "relationship" dataframe
-# For every song, take its song_id and iterate over its playlist_id to create
-# as many rows as needed. E.g. for "A Sky Full of Stars - Hardwell Remix" we should create
+# E.g. for "A Sky Full of Stars - Hardwell Remix" we should create
 # 4 rows of [1066, 150], [1066, 330], [1066, 1712], and [1066, 1810]
 # this can be done easily using pd.explode()
 
-relationships = tracks_unique.explode("playlist_id")
-relationships = relationships.drop(columns=["track_name", "artist_name"])
+relationships = tracks[["song_id", "playlist_id"]]
+relationships = relationships.explode("playlist_id", ignore_index=True)
 print(relationships)
 print(relationships[relationships["song_id"] == 1066])
 print(relationships.info())
 
-# To import relationships, now we all have to do is use this dataframe——the song_id and
-# playlist_id columns represent our links (e.g. song_id = 3 and playlist_id = 0 means
-# the 4th song is in the 1st playlist)
+# To import relationships into Neo4j, now we all have to do is use this dataframe——
+# the song_id and playlist_id columns represent our links (e.g. song_id = 3 and
+# playlist_id = 0 means the 4th song is in the 1st playlist)
 
 # However, this is possibly not done. We may still want to do some cleaning to remove
 # metadata for memory reasons——maybe playlist descriptions are unnecessary
